79c144028758241e5118485e286e0e819784d005
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-11-24 11:03:23 -0600

    Log-3

--- PhoneBook/Person.java
+++ PhoneBook/Person.java
@@ -1,5 +1,49 @@
 package PhoneBook;
 
 public class Person {
+    private String name;
+    private String address;
+    private String state;
+    private int zipCode;
+
+    public Person(String name, String address, String state, int zipCode) {
+        super();
+        this.name = name;
+        this.address = address;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
 
 }
